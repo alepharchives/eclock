a43e70c4be46e5c3150c8d42dc2aa084f4a6c88e
Marcos Almonacid <amilcar.54@gmail.com>
2011-08-04 12:00:14 -0300

    Added new process and stop ability.

--- src/eclock.erl
+++ src/eclock.erl
@@ -1,6 +1,6 @@
 -module(eclock).
 
--export([run_interval/3, test/0]).
+-export([run_interval/3, spawn_interval/2, loop/2, stop/1 ,test/0, test2/0]).
 
 run_interval(_Task, _Interval, 0) ->
 	ok;
@@ -9,5 +9,25 @@ run_interval(Task, Interval, Times) ->
 	Task(),
 	run_interval(Task, Interval, Times - 1).
 
+spawn_interval(Task, Interval) ->
+	spawn(?MODULE, loop, [Task, Interval]).
+
+loop(Task, Interval) ->
+	receive
+		stop ->
+			ok
+	after Interval*1000 ->
+		Task(),
+		loop(Task, Interval)
+	end.
+
+stop(Clock) ->
+	Clock ! stop.
+
 test() ->
 	io:format("Testing eclock ~n").
+
+test2() ->
+	io:format("second process~n").
+
+
