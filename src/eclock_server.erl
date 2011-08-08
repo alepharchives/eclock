b7da0abe2b1117b62d105a4efafe70bfb7d8f006
Marcos Almonacid <amilcar.54@gmail.com>
2011-08-08 16:25:21 -0300

    Changed create server function from 'start/3' to 'start_link/3'.

--- src/eclock_server.erl
+++ src/eclock_server.erl
@@ -13,7 +13,7 @@
 
 %% --------------------------------------------------------------------
 %% External exports
--export([start/2, stop/1]).
+-export([start_link/2, stop/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -23,8 +23,8 @@
 %% ====================================================================
 %% External functions
 %% ====================================================================
-start(Task,Interval) ->
-	gen_server:start(?MODULE,[Task, Interval], []).
+start_link(Taskk,Interval) ->
+	gen_server:start_link(?MODULE,[Taskk, Interval], []).
 
 stop(Pid) ->
 	gen_server:cast(Pid, stop).
